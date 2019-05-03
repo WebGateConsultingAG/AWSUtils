ca0fb0cebec48959ecc64a8ff20ae7cc30c89120
Christian Guedemann <christian.guedemann@webgate.biz>
2019-05-03 19:42:19 +0200

    Version 0.5.0

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,15 @@ import { Callback } from 'aws-lambda';
 
 import { Done } from './lib/done';
 import { CognitoUser } from './lib/CognitoUser';
+import { DocumentClient } from 'aws-sdk/clients/dynamodb';
+import { DocumentClientWrapper } from './lib/documentClientWrapper';
 
 export function done(cb: Callback, headers: object) {
   return new Done(cb, headers);
 }
 export function cognitoUser(authorizer: any) {
   return new CognitoUser(authorizer);
+}
+export function dynamoDocClient(client: DocumentClient) {
+  return new DocumentClientWrapper(client);
 }
